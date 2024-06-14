c3ebb1f66bbd675f2bce6862f8c575e9b861facb
leandrufleidl <leandrufleidl@gmail.com>
2024-06-14 16:27:24 +0200

    Update README.md

--- AdultMortallityAsFunctionOfTime.R
+++ AdultMortallityAsFunctionOfTime.R
@@ -27,4 +27,34 @@ aMortplot = ggplot(b.A_mortality, aes(x = mean_temp, y = mortality) ) +
                                  )
 aMortplot
 
-                                        
+#3 Temperature dependent function as seen on page 5/18 0f lord et al
+adult_mortalityFunc = function(con.a1, con.a2, temp){
+  if (temp > 25){
+    mortality.rate = con.a1*exp((con.a2)*(temp - 25))
+  }else{
+    mortality.rate = con.a1
+  }
+  return(mortality.rate)
+}
+# 4 Fit to a nonlinear curve using adult_mortalityFunc as our nonlinear curve and
+# using nls.lm to minimize the difference between our real and expected value.
+amFunctFit = function(params, dat = b.A_mortality){
+           a1 = params[1]
+           a2 = params[2]
+           temp = dat$mean_temp
+           mod.mort <- sapply(temp,function(x){ adult_mortalityFunc(    
+              con.a1=a1
+             ,con.a2=a2
+             ,x)
+           })
+           diffrence = log(mod.mort) - log(dat$mortality)
+           return(diffrence)
+}
+
+adult_mortalityFit = nls.lm(par = c(a1 = 0.01, a2 = 0.06),  fn = amFunctFit)
+
+#5 Store the paramaters we found for later use
+amParams = summarise(adult_mortalityFit)
+a1 = coef(adult_mortalityFit)[1]
+a2 = coef(adult_mortalityFit)[2]
+
