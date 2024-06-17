4cfb8a4c2258b453e9a070fc6fdd1df283172fb3
leandrufleidl <leandrufleidl@gmail.com>
2024-06-17 12:28:39 +0200

    Update README.md

---
+++ LarvipostionAsFunctionOfTime.R
@@ -0,0 +1,64 @@
+# Fit larviposition as a function of time, the data used is found in:
+# ~ Hargrove (1994) Reproductive Rates of Tsetse Flies in the the 
+# field of Zimbabwe
+# ~ Physiological Entomology vol. 19 (4). 307-318
+# ~ Hargrove (2004) Tsetse Population Dynamics In the Trypanosiomiases 
+
+require(ggplot2)
+#1 Read in data
+d.larvi.rate = read.csv("data_larviposition.csv")
+
+#2 Plot data
+larvi_plot = ggplot(d.larvi.rate, aes(x = mean_temp, y = rate, col = stage)) +
+             scale_color_manual(values = c("black", "black", "red", "red")) +
+             xlim(low = 13, high = 33) +
+             geom_point(size = 1) +
+             ylim(low = 0, high=0.14) +
+             labs(y = "Larvi Rate", x = "Mean Temp,", title = "Larvipostion Rate") +
+             theme_set(theme_bw()) +
+             theme(panel.border = element_blank()
+                   ,plot.title = element_text(hjust = 0.5)
+                   , axis.line = element_line(color = "black")
+                   , legend.position = c(0.9, 0.1)
+                   , legend.title = element_blank()
+                   , legend.background = element_blank()
+                   , legend.key.size = unit(0.5, "line")
+                   , legend.text = element_text(size = 4.5)
+                   , legend.key.height = unit(0.4, "line")
+                  )
+ larvi_plot
+  
+#3  Create temperature dependent function
+ 
+ lrFunc = function(con.d1, con.d2, T1 = 24, temp){
+         lr = con.d1 + con.d2*(temp - T1)
+         return(lr)
+ }
+ 
+#4  Save our constants for both subsequent and non_subsequent larviposition
+ non_sub.d1 = 0.061
+ non_sub.d2 = 0.002
+ sub.d1 = 0.1046
+ sub.d2 = 0.0052
+ 
+#5 Use function to create the predicted value dataframes
+ non_sub.lr = lrFunc(con.d1 = non_sub.d1, con.d2 =non_sub.d2, temp = seq(15, 34, 0.1))
+ non_sub.lr = cbind.data.frame(temp = seq(15, 34, 0.1)
+                              , pred = non_sub.lr
+                              , stage = rep("Non Subsequent Larvi Fit")
+                              , length(non_sub.lr))
+ 
+ sub.lr = lrFunc(con.d1 = sub.d1, con.d2 = sub.d2, temp = seq(15, 34, 0.1))
+ sub.lr = cbind.data.frame(temp = seq(15, 34, 0.1)
+                           , pred = sub.lr
+                           , stage = rep("Subsequent Larvi Fit")
+                           , length(non_sub.lr))
+ prediction.lr = rbind.data.frame(non_sub.lr, sub.lr)
+ 
+#5 Add prediction to plot
+
+ lrPlotFit <- larvi_plot +
+   geom_line(data=prediction.lr
+             ,mapping=aes(x=temp,y=pred))
+ lrPlotFit
+ 
--- README.md
+++ README.md
@@ -4,10 +4,13 @@ This is a reproduction of the code used for the paper
 > <span xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">Lord J.S., Hargrove J.W., Torr S.J. and Vale, G.A. (2018). Climate change and African trypanosomiasis vector populations in Zimbabwe's Zambezi Valley: a mathematical modelling study.
 PLoS Medicine. https://doi.org/10.1371/journal.pmed.1002675.
 
-I intend to add some code later to project th Tsetse fly population of the Kruger National park
-however the majority of the work I have done simply to understand the work done by lord et al.
+I intend to add some code later to project the Tsetse fly population of the Kruger National park
+however the majority of the work I have done is simply to better understand the work done by lord et al.
 
 CountAndTemp.R reads in the temperature data and the tsetse fly count and combines them into
 a useful database.
 
 AdultMortalityAsFunctionOfTime.R reads in adult mortality data and plots it along with the predicted mortality as a function of time.
+
+LarvipostionAsFunctionOfTime.R Reads in larviposition data and plots it along with the predicted
+larviposition as a function of time.
